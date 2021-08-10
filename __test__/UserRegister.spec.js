8aeff6ee274ae922a384a4184ecfda2c3047212a
Maths Physis <smathsphysis@gmail.com>
2021-08-09 21:10:43 -0400

    feat: tests and implementation for user registration feature

---
+++ __test__/UserRegister.spec.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('User Registration', () => {
+  it('returns 200 Ok when signup request is valid', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@email.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('returns success message when signup request is valid', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@email.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+});
