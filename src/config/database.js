b3b85ef40966074fd2b3d7c7ed90a202a9f643ed
Maths Physis <smathsphysis@gmail.com>
2021-08-09 22:03:55 -0400

    feat: DB sync on server and logging turned off

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
   dialect: 'sqlite',
   storage: './database.sqlite',
+  loggin: false,
 });
 
 module.exports = sequelize;
