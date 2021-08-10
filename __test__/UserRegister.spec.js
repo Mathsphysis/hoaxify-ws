343ef014baabbcc941d36fe87c74ed969b18b3ec
Maths Physis <smathsphysis@gmail.com>
2021-08-09 21:41:16 -0400

    feat: test, model and db config for User

--- __test__/UserRegister.spec.js
+++ __test__/UserRegister.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/user');
 
 describe('User Registration', () => {
   it('returns 200 Ok when signup request is valid', (done) => {
@@ -27,6 +28,22 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('returns success message when signup request is valid', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@email.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
       });
   });
 });
