6d87ca82949c9030459e6d850afeda46de15121d
Maths Physis <smathsphysis@gmail.com>
2021-08-11 03:19:20 -0400

    refactor: Confirms that the password is hashed and refactored dynamic test

--- __test__/UserRegister.spec.js
+++ __test__/UserRegister.spec.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const bcrypt = require('bcrypt');
 const app = require('../src/app');
 const User = require('../src/user/user');
 const sequelize = require('../src/config/database');
@@ -41,26 +42,33 @@ describe('User Registration', () => {
     expect(savedUser.email).toBe('user1@mail.com');
   });
 
-  it('confirms that the password is not plainly saved', async () => {
+  it('confirms that the password is correctly hashed', async () => {
     await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
-    expect(savedUser.username).not.toBe('P4ssword');
+    bcrypt.compare(validUser.password, savedUser.password, (err, result) => {
+      expect(err).toBeUndefined();
+      expect(result).toBe(true);
+    });
   });
 
-  it.each([
-    ['username', 'Username cannot be null'],
-    ['email', 'Email cannot be null'],
-    ['password', 'Password cannot be null'],
-  ])('when %s is null, %s is received', async (field, expectedMessage) => {
-    const invalidUser = { ...validUser };
-    invalidUser[field] = null;
-    const response = await postUser(invalidUser);
-    expect(response.status).toBe(400);
-    const { validationErrors } = response.body;
-    expect(validationErrors).not.toBeUndefined();
-    expect(validationErrors[field]).toBe(expectedMessage);
-  });
+  it.each`
+    field         | expectedMessage
+    ${'username'} | ${'Username cannot be null'}
+    ${'email'}    | ${'Email cannot be null'}
+    ${'password'} | ${'Password cannot be null'}
+  `(
+    'returns $expectedMessage when $field is null',
+    async ({ field, expectedMessage }) => {
+      const invalidUser = { ...validUser };
+      invalidUser[field] = null;
+      const response = await postUser(invalidUser);
+      expect(response.status).toBe(400);
+      const { validationErrors } = response.body;
+      expect(validationErrors).not.toBeUndefined();
+      expect(validationErrors[field]).toBe(expectedMessage);
+    }
+  );
 
   it('returns validationErrors for all invalid fields', async () => {
     const invalidUser = { ...validUser };
