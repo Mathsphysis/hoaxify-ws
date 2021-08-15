a362df175b352184b93b96a7a503e76112718958
Maths Physis <smathsphysis@gmail.com>
2021-08-14 20:38:49 -0400

    feat: Added activationToken to user service

--- src/user/userService.js
+++ src/user/userService.js
@@ -1,11 +1,23 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const User = require('./user');
+
+const ACTIVATION_TOKEN_LENGTH = 16;
+
+const generateToken = (length) =>
+  crypto.randomBytes(length / 2).toString('hex');
 
 const save = async (body) => {
   const { username, email, password, inactive } = body;
 
   const hash = await bcrypt.hash(password, 10);
-  const user = { username, email, password: hash, inactive };
+  const user = {
+    username,
+    email,
+    password: hash,
+    inactive,
+    activationToken: generateToken(ACTIVATION_TOKEN_LENGTH),
+  };
   const savedUser = await User.create(user);
   return savedUser;
 };
