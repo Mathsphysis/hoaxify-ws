0b6b8bb821853de1be1f8494bc5757c4035f8ce7
Maths Physis <smathsphysis@gmail.com>
2021-08-18 20:32:45 -0400

    fix: email service mail message

--- src/email/EmailService.js
+++ src/email/EmailService.js
@@ -1,12 +1,23 @@
 const transporter = require('../config/emailTransporter');
 
 const sendAccountActivation = async (user) => {
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: 'My App <info@my-app.com>',
     to: `${user.username} <${user.email}>`,
     subject: 'account_activation',
-    html: `Token is ${user.activationToken}`,
+    html: `
+    <div>
+      <b>Please click the link below to activate your account</b>
+    </div>
+    <div>
+      <a href="http://localjost:8080/#/login?token=${user.activationToken}">Activate</a>
+    </div>
+    Token is ${user.activationToken}`,
   });
+  if (process.env.NODE_ENV === 'dev') {
+    // eslint-disable-next-line no-console
+    console.log(info);
+  }
 };
 
 module.exports = {
